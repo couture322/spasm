d82a05fa551ae379785bd2a956f2c75c7122fab9
Jessica Couture <jcouture@ESM.UCSB.EDU>
2019-08-27 16:58:13 -0700

    Edits notLeave in sim_fishAq. Adds multi-param runs to icesPlots

--- R/sim_fisheryAq.R
+++ R/sim_fisheryAq.R
@@ -534,7 +534,7 @@ sim_fisheryAq <-
     #buffMov[bufferLocs]  <- ifelse(is.na(bufferMove),1,bufferMove)
 
     edge<-which(diff(mpa_locations)!=1)
-    notLeave <- expand.grid(to=c(mpa_locations[edge]+1,mpa_locations[edge]-(farmSize+1),max(mpa_locations)+1,mpa_locations[max(edge+1)]-1),
+    notLeave <- expand.grid(to=c(mpa_locations[edge]+(1:buffSize),mpa_locations[edge]-(farmSize+(1:buffSize)),max(mpa_locations)+(1:buffSize),mpa_locations[max(edge+1)]-(1:buffSize)),
                             from=mpa_locations)%>% # limit attraction to areas close (within buffsize) to the farm (each patch of the farm)
       as.data.frame()%>%
       mutate(noLve=0.7) ### propensity to STAY
@@ -590,8 +590,8 @@ sim_fisheryAq <-
           adult_move_grid <- adult_move_grid %>%
             ungroup()%>%
             left_join(.,farm_stay)%>% # merge attractor values with the 'to' patches
-            left_join(.,notLeave,by=c(to,from))%>%
-            mutate(farmImpacts=ifelse(is.na(noLve,farmImpacts,noLve)),
+            left_join(.,notLeave,by=c("to","from"))%>%
+            mutate(farmImpacts=ifelse(is.na(noLve),farmImpacts,noLve),
                    farmImpacts=replace_na(farmImpacts,1),
                    prob_move=prob_move*farmImpacts) %>% ## adjust 'prob_move' by 'farm_stay'; increase probabilty that move FROM farm locations
             select(-noLve)
