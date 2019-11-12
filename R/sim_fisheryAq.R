f3a9baba0bf39e4dc48ed7a392fb8575add52ee1
Jessica Couture <jcouture@ESM.UCSB.EDU>
2019-11-12 11:31:13 -0800

    Indexes the farms differently for the spacing of the farms, so that a large farm (>50%) can still fit in the patch area

--- R/sim_fisheryAq.R
+++ R/sim_fisheryAq.R
@@ -271,7 +271,7 @@ sim_fisheryAq <-
 
       nFrms<-round(prop_mpas/farmSize)
 
-      farmStart<-round(seq(0,num_patches,length.out = nFrms+2))[1:nFrms+1]
+      farmStart<-round(seq(0,num_patches,length.out = nFrms+2))[0:nFrms]
 
       farmLFunc<-function(x){
         seq(x,(x+farmSize-1),by=1)
